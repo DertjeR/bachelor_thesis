91ba8147dc11bb09647c493c81c54a524cd6a413
DertjeR <d.j.roggeveen@student.rug.nl>
2024-10-25 14:46:25 +0200

    basic software outline

---
+++ main.py
@@ -0,0 +1,18 @@
+import sys
+from typing import Dict, List, Union, Mapping
+from rsa import *
+
+def get_message() -> str:
+    pass
+
+def preprocess() -> List[str]:
+    pass
+
+def char_to_octal() -> List[int]:
+    pass
+
+def octal_to_numbers() -> List[Dict[int, int]]:
+    pass
+
+def rsa() -> int:
+    pass
---
+++ rsa.py
@@ -0,0 +1,12 @@
+from typing import Dict, List, Union, Mapping
+
+
+def calculate_keys() -> List[int]:
+    pass
+
+def encryption() -> int:
+    pass
+
+def decryption() -> str:
+    pass
+
